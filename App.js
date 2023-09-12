722cb92667d236d3b99f4a50d8cb6023b94179bf
Chittepu1991 <venkatesh.02052012@gmail.com>
2023-09-12 14:20:20 +0530

    first

---
+++ App.js
@@ -0,0 +1,72 @@
+import { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./src/Components/Header";
+import Body from "./src/Components/Body";
+//import About from "./components/About";
+import Contact from "./src/Components/Contact";
+import Error from "./src/Components/Error";
+import RestaurantMenu from "./src/Components/RestaurantMenu";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+//import Grocery from "./components/Grocery";
+
+// Chunking
+// Code Splitting
+// Dynamic Bundling
+// lazy Loading
+// on demand loading
+// dynamix imoprt
+
+const Grocery = lazy(() => import("./src/Components/Grocery"));
+
+const About = lazy(() => import("./src/Components/About"));
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+// Footer component for footer section
+const Footer = () => {
+    return (
+      <div className="footer">
+        <h1>Mani</h1>
+      </div>
+    );
+  };
+  
+  export default Footer;
